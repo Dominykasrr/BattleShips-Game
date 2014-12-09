6b0daceeb97728683dccfe03bd037532fd68cee4
Santi <deewan_me1999@yahoo.com>
2014-12-10 00:45:02 +0100

    Error

--- BattleShipService/BattleShipClient/Form1.cs
+++ BattleShipService/BattleShipClient/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace BattleShipClient
 {
-    public partial class Form1 : Form, ServiceReference1.IPortal
+    public partial class Form1 : Form
     {
         ServiceReference1.PortalClient proxy;
         public Form1()
