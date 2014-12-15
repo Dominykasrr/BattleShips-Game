d2ca21788f36306a6a2350fad0727c4b20f663ef
Santi <deewan_me1999@yahoo.com>
2014-12-15 16:23:59 +0100

    Fresh one

--- BattleShipService/BattleShipService/GameService.cs
+++ BattleShipService/BattleShipService/GameService.cs
@@ -7,7 +7,6 @@ using System.Text;
 
 namespace BattleShipService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class GameService : IPortal
     {
